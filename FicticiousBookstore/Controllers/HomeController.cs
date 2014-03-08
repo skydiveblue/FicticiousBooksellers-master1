202357168172e8e3387a09579599d3f1e1b9716d
skydiveblue <skydiveblue@gmail.com>
2014-03-07 20:54:55 -0700

    Upload

---
+++ FicticiousBookstore/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using FicticiousBookstore.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace FicticiousBookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var fromAddress = "ficticiousbooksellers@gmail.com";
+
+                using (var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress, "fiction1234")
+                })
+                {
+                    using (var message = new MailMessage(fromAddress, fromAddress)
+                        {
+                            Subject = model.Subject,
+                            Body = model.Email + " sent you the following message:\n\n" + model.Message
+                        })
+                    {
+                        smtp.Send(message);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FicticiousBookstore/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FicticiousBookstore.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(800)]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
